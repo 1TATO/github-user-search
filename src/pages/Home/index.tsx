205fec6f10aaa8fedec627a096dfd66732c4dec9
José Neto <josenetopr5@gmail.com>
2021-01-29 13:52:04 -0300

    Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,53 @@
 import React from 'react';
+import { FiLogOut } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-import {Container} from './styles';
+import { Header, Profile, Info, Social, Bio } from './styles';
 
 const Home: React.FC = () => {
   return (
-    <Container>
+    <>
+      <Header>
+        <p>#anilton.veiga</p>
 
-      <h1>Home</h1>
-    </Container>
+        <Link to="/">
+          Sair
+          <FiLogOut />
+        </Link>
+      </Header>
+
+      <Profile>
+        <img src="https://avatars.githubusercontent.com/u/42624981?s=460&u=188dc2e1e9964c8008135f43468f0db8db3cf70b&v=4" alt="" />
+
+        <Info><h2>ANILTON VEIGA</h2>
+          <p>anilton.veiga@gmail.com</p>
+          <p>Itai/SP</p>
+        </Info>
+
+        <Social>
+          <Link to="#">
+            <span>32</span>
+            <span>Seguidores</span>
+          </Link>
+
+          <Link to="#">
+            <span>32</span>
+            <span>Seguindo</span>
+          </Link>
+
+          <Link to="#">
+            <span>10</span>
+            <span>Repos</span>
+          </Link>
+        </Social>
+
+        <Bio>
+          <h2>BIO</h2>
+
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptas vitae vero, obcaecati, molestiae sint cupiditate nemo dicta sit saepe, dignissimos possimus enim iure repellendus laudantium doloremque assumenda iusto minus!</p>
+        </Bio>
+      </Profile>
+    </>
   )
 }
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,106 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Header = styled.header`
+  width: 100%;
+  height: 200px;
+  background: #1F1F1F;
+  color: #FFFFFF;
+
+  padding: 25px;
+  display: flex;
+  justify-content: space-between;
+
+  p {
+    font-weight: bold;
+  }
+
+  a {
+    font-weight: 300;
+    color: #FFFFFF;
+    text-decoration: none;
+
+    svg {
+      color: #D03434;
+      margin-left: 12px;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  text-align: center;
+  color: #fff;
+
+  img {
+    border-radius: 50%;
+    height: 170px;
+    width: 170px;
+    margin-top: -80px;
+    border: solid;
+  }
+`;
+
+export const Info = styled.div`
+  text-align: left;
+  margin-top: 45px;
+
+  h2 {
+    font-size: 26px;
+    margin-bottom: 5px;
+    text-transform: uppercase;
+  }
+
+  p {
+    font-weight: 300;
+    font-size: 18px;
+
+    & + p {
+      margin-top: 5px;
+    }
+  }
+`;
+
+export const Social = styled.div`
+  margin-top: 45px;
+  text-align: left;
+  height: 100px;
+  background: #5252525D;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px 40px 12px;
+
+  a {
+    display: flex;
+    flex-direction: column;
+    text-decoration: none;
+    color: #fff;
+    text-align: center;
+
+    span {
+      font-weight: bold;
+      font-size: 40px;
+
+      & + span {
+        font-weight: 300;
+        font-size: 17px;
+      }
+    }
+  }
+`;
+
+export const Bio = styled.div`
+  text-align: left;
+  margin-top: 53px;
+
+  h2 {
+    font-size: 26px;
+    margin-bottom: 15px;
+    text-transform: uppercase;
+  }
+
+  p {
+    font-size: 18px;
+    font-weight: 300;
+  }
+`;
