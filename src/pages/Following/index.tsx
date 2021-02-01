5d9a552321e315ea2c409a9b3c29b1168cd0e764
José Neto <josenetopr5@gmail.com>
2021-01-31 22:23:21 -0300

    Following page

---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import { FiArrowRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import UserHeader from '../../components/UserHeader';
+
+import { FollowingContainer } from './styles';
+
+interface UserParams {
+  login: string;
+}
+
+interface UserData {
+  login: string;
+  avatar_url: string;
+  following: number;
+}
+
+interface FollowData {
+  id: string;
+  login: string;
+  avatar_url: string;
+}
+
+const Followers: React.FC = () => {
+  const { params } = useRouteMatch<UserParams>();
+
+  const [data, setData] = useState<UserData>();
+  const [follows, setFollows] = useState<FollowData[]>([]);
+
+  useEffect(() => {
+    api.get(`users/${params.login}`).then(response => {
+      setData(response.data);
+    });
+  }, [params.login]);
+
+  useEffect(() => {
+    api.get(`users/${params?.login}/following`).then(response => {
+      setFollows(response.data);
+    });
+  }, [params?.login]);
+
+  return (
+    <>
+      <UserHeader>
+        <p>{data?.following} seguindo</p>
+      </UserHeader>
+
+      {
+        follows.map((follow) => (
+          <FollowingContainer key={follow.id}>
+            <img src={follow.avatar_url} alt={follow.login} />
+            <p>#{follow.login}</p>
+            <FiArrowRight />
+          </FollowingContainer>
+        ))
+      }
+    </>
+  )
+}
+
+export default Followers;
---
+++ src/pages/Following/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const FollowingContainer = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  position: relative;
+  color: #fff;
+  border-bottom: 1px solid #7070705A;
+
+  img {
+    margin: 20px 30px 25px;
+    border-radius: 50%;
+    border: 2px solid #fff;
+    height: 65px;
+    width: 65px;
+  }
+
+  p {
+    flex: 1;
+    font-weight: bold;
+  }
+
+  svg {
+    margin-right: 50px;
+    height: 30px;
+    width: 30px;
+  }
+
+  &::before {
+    content: '';
+    height: 42px;
+    width: 10px;
+    position: absolute;
+    background: #FFCE00;
+    left: 0;
+    border-radius: 0 100px 100px 0;
+  }
+`;
