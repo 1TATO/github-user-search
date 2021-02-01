e1862ef6c0884c31655204414af1861d56336a27
José Neto <josenetopr5@gmail.com>
2021-01-31 22:21:24 -0300

    Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Home from '../pages/Home';
+import Followers from '../pages/Followers';
+import Following from '../pages/Following';
+import Repositories from '../pages/Repositories';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Login} />
     <Route path="/home/:login" component={Home} />
+    <Route path="/:login/followers" component={Followers} />
+    <Route path="/:login/following" component={Following} />
+    <Route path="/:login/repos" component={Repositories} />
   </Switch>
 )
 
