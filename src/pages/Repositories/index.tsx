273b6f0654495b710af69862b1a0f6152114e5ae
José Neto <josenetopr5@gmail.com>
2021-01-31 22:22:33 -0300

    Repositories page connected to API

--- src/pages/Repositories/index.tsx
+++ src/pages/Repositories/index.tsx
@@ -1,30 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 import { FiArrowLeft, FiStar, FiUnlock, FiLock } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+import UserHeader from '../../components/UserHeader';
+
 import { DetailsHeader, RepositoriesContainer, Actions, Stars, Locks } from './styles';
 
+interface UserParams {
+  login: string;
+}
+
+interface UserData {
+  login: string;
+  avatar_url: string;
+  public_repos: number;
+}
+
+interface RepositoryData {
+  id: string;
+  name: string;
+  description: string;
+  stargazers_count: number;
+}
+
 const Repositories: React.FC = () => {
+  const { params } = useRouteMatch<UserParams>();
+
+  const [data, setData] = useState<UserData>();
+  const [repositories, setRepositories] = useState<RepositoryData[]>([]);
+
+  useEffect(() => {
+    api.get(`users/${params.login}`).then(response => {
+      setData(response.data);
+    });
+  }, [params.login]);
+
+  useEffect(() => {
+    api.get(`users/${params.login}/repos`).then(response => {
+      setRepositories(response.data);
+    });
+  }, [params.login]);
+
   return (
     <>
-      <DetailsHeader>
-        <FiArrowLeft />
-        <p>10 repositorios</p>
-      </DetailsHeader>
+      <UserHeader>
+        <p>{data?.public_repos} repositórios</p>
+      </UserHeader>
 
-      <RepositoriesContainer>
-        <h2>brasiliapp-react-native</h2>
-        <p>Repository for centralization of the BrasiliApp mobile project</p>
-        <Actions>
-          <Stars>
-            <FiStar />
-            <span>32</span>
-          </Stars>
-          <Locks>
-            <FiUnlock />
-            <FiLock />
-          </Locks>
-        </Actions>
-      </RepositoriesContainer>
+      {
+        repositories.map((repository) => (
+          <RepositoriesContainer key={repository.id}>
+            <h2>{repository.name}</h2>
+            <p>{repository.description}</p>
+            <Actions>
+              <Stars>
+                <FiStar />
+                <span>{repository.stargazers_count}</span>
+              </Stars>
+              <Locks>
+                <FiUnlock />
+                <FiLock />
+              </Locks>
+            </Actions>
+          </RepositoriesContainer>
+        ))
+      }
     </>
   )
 }
